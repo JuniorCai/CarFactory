eb84c2ffb629818107862de0d2fc1b7e89877ecb
JuniorCai <JuniorCai@outlook.com>
2018-03-19 15:56:56 +0800

    添加公司管理页面

---
+++ CarFactory.Admin/Controllers/CompanyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abp.Application.Navigation;
+using CarFactory.Application.Company;
+
+namespace CarFactory.Admin.Controllers
+{
+    public class CompanyController : CarFactoryControllerBase
+    {
+
+        private readonly ICompanyAppService _companyAppService;
+
+        public CompanyController(ICompanyAppService companyAppService, IUserNavigationManager userNavigationManager) : base(userNavigationManager)
+        {
+            _companyAppService = companyAppService;
+        }
+
+
+        // GET: Company
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
