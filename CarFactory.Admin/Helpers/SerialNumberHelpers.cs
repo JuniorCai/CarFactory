d63e2476709cfa86af6d459b6b34b089fd90697d
JuniorCai <JuniorCai@outlook.com>
2018-03-28 09:56:50 +0800

    1.修改首页产品块图片拉伸问题 2.添加伪序号随机数方法

---
+++ CarFactory.Admin/Helpers/SerialNumberHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarFactory.Admin.Helpers
+{
+    public class SerialNumberHelpers
+    {
+        private static int _minValue = 111111;
+        private static int _maxValue = 999999;
+
+        public static int GenerateSerialNumber()
+        {
+            Random serialRandom = new Random();
+            return serialRandom.Next(_minValue, _maxValue);
+        }
+    }
+}
